d4a4f3eb84c2b2e005f26648a3b76dad504422f4
Ayşegül Öztürk <69517016+aysegul-ozturk@users.noreply.github.com>
2024-10-29 22:39:34 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,11 @@
-import scapy
+from scapy.all import sniff, ICMP
 
-# Implement your ICMP receiver here
+def process_packet(packet):
+    if packet.haslayer(ICMP):
+        packet.show()
+
+def receive_icmp():
+    sniff(filter="icmp", prn=process_packet)
+
+if __name__ == "__main__":
+    receive_icmp()
